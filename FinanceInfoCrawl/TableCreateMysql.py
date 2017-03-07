f2e485f7e534bad2fcf175bad63fbb8aa4ff00bb
toyaji <happytoday83@naver.com>
2017-03-07 17:21:07 +0900

    20170307 Stock Price history fetching                # todo 데이터 받다가 끊어졌는데 나중에 다시 시도해보기

--- FinanceInfoCrawl/TableCreateMysql.py
+++ FinanceInfoCrawl/TableCreateMysql.py
@@ -33,6 +33,25 @@ def create_tables(tab):
 def showtable(db):
     pass
 
+def table_drop(tab):
+    """ create tables in the SQL database"""
+    commands ="DROP TABLE IF EXISTS %s;"
+    conn = None
+    try:
+        # connect to the PostgreSQL server
+        conn = pymysql.connect(database='kospi', user='root', port=3306, password='1234')
+        cur = conn.cursor()
+        # create table one by one
+        cur.execute(commands % tab)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, pymysql.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
 
 
 if __name__ == '__main__':
